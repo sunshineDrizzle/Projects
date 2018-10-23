ef8e53153acacb49cd8d95c751aae4396ba92498
sunshineDrizzle <954830460@qq.com>
2018-10-23 16:43:18 +0800

    Refine: change method of get copes

--- FFA_action_pattern_analysis/tmp_tool/get_copes.py
+++ FFA_action_pattern_analysis/tmp_tool/get_copes.py
@@ -11,16 +11,19 @@ if __name__ == '__main__':
                                     (20, 'PLACE-AVG'),
                                     (21, 'TOOL-AVG')])
     subject_id_file = '/nfs/t3/workingshop/chenxiayu/study/FFA_clustering/data/HCP_face-avg/s2/subject_id'
-    src_files = '/nfs/s2/userhome/chenxiayu/workingdir/data/HCP/S1200_WM_all_cope_s4/' \
-                '{}_tfMRI_WM_level2_hp200_s4_MSMAll.dscalar.nii'
+    src_files = '/nfs/s2/userhome/chenxiayu/workingdir/data/HCP/S1200_WM_all_cope_s2/' \
+                '{}_tfMRI_WM_level2_hp200_s2_MSMAll.dscalar.nii'
     trg_file = '/nfs/t3/workingshop/chenxiayu/study/FFA_clustering/data/' \
-               'S1200_1080_WM_interested_copes_s4_MSMAll_32k_fs_LR.dscalar.nii'
+               'S1200_1080_WM_cope{0}_{1}_s2_MSMAll_32k_fs_LR.dscalar.nii'
 
     with open(subject_id_file) as rf:
         subject_ids = rf.read().splitlines()
 
-    maps_new = []
-    map_names_new = []
+    maps_new_dict = OrderedDict()
+    map_names_new_dict = OrderedDict()
+    for k in interested_copes.keys():
+        maps_new_dict[k] = []
+        map_names_new_dict[k] = []
     brain_models = None
     for subject_id in subject_ids:
         src_file = src_files.format(subject_id)
@@ -36,8 +39,10 @@ if __name__ == '__main__':
                 raise RuntimeError("subject-{0}'s cope{1} is not {2}".format(subject_id, k, v))
 
         for k in interested_copes.keys():
-            maps_new.append(maps[k])
-            map_names_new.append(map_names[k])
+            maps_new_dict[k].append(maps[k].copy())
+            map_names_new_dict[k].append(map_names[k])
             print('Finished: merge {0}_cope{1}'.format(subject_id, k))
 
-    save2cifti(trg_file, np.array(maps_new), brain_models, map_names_new)
+    for k in interested_copes.keys():
+        save2cifti(trg_file.format(k, interested_copes[k]),
+                   np.array(maps_new_dict[k]), brain_models, map_names_new_dict[k])
