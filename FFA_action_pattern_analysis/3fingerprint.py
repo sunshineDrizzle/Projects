a2d8611f3b609a6de18afd996c7ecef1fe739fd3
sunshineDrizzle <954830460@qq.com>
2018-10-31 21:38:15 +0800

    Refine: neaten codes

--- FFA_action_pattern_analysis/3fingerprint.py
+++ FFA_action_pattern_analysis/3fingerprint.py
@@ -9,13 +9,12 @@ if __name__ == '__main__':
     # predefine some variates
     # -----------------------
     # predefine parameters
-    n_clusters = 3
-    subproject_name = '2mm_KM_init10_regress_right'
     brain_structure = 'CIFTI_STRUCTURE_CORTEX_RIGHT'
     interested_copes = OrderedDict([(18, 'BODY-AVG'),
                                     (19, 'FACE-AVG'),
                                     (20, 'PLACE-AVG'),
                                     (21, 'TOOL-AVG')])
+    rois = ['r1_FFA_m', 'r2_FFA_p', 'r2_FFA_a', 'r3_FFA_p', 'r3_FFA_a']
     roi2label = {'r1_FFA_m': '3',
                  'r2_FFA_p': '1',
                  'r2_FFA_a': '1',
@@ -23,12 +22,9 @@ if __name__ == '__main__':
                  'r3_FFA_a': '2'}
 
     # predefine paths
-    working_dir = '/nfs/s2/userhome/chenxiayu/workingdir'
-    project_dir = pjoin(working_dir, 'study/FFA_clustering')
-    subproject_dir = pjoin(project_dir, subproject_name)
-    n_clusters_dir = pjoin(subproject_dir, '{}clusters'.format(n_clusters))
-    # FFA_label = pjoin(project_dir, 'data/HCP_face-avg/label/rFFA_2mm.label')
-    FFA_label = pjoin(n_clusters_dir, '{}.label')
+    project_dir = '/nfs/s2/userhome/chenxiayu/workingdir/study/FFA_clustering'
+    n_clusters_dir = pjoin(project_dir, '2mm_KM_init10_regress_right/3clusters')
+    roi_file = pjoin(n_clusters_dir, '{}.label')
     cope_files = pjoin(project_dir, 'data/S1200_1080_WM_cope{0}_{1}_s2_MSMAll_32k_fs_LR.dscalar.nii')
     subject_labels_path = pjoin(n_clusters_dir, 'subject_labels')
     # -----------------------
@@ -39,29 +35,33 @@ if __name__ == '__main__':
 
     # analyze labels
     # --------------
-    cope_titles = ['{}_FFA_mean'.format(v) for v in interested_copes.values()]
     cope_dict = dict()
-    for roi in roi2label.keys():
+    for roi in rois:
         cope_dict[roi] = dict()
     for k, v in interested_copes.items():
         cope_file = cope_files.format(k, v)
         cope_data = CiftiReader(cope_file).get_data(brain_structure, True)
-        for roi, label in roi2label.items():
-            FFA_vertices = nib.freesurfer.read_label(FFA_label.format(roi))
-            cope_data_FFA = cope_data[:, FFA_vertices]
-            # get subgroup data
-            sub_cope_data_FFA = np.atleast_2d(cope_data_FFA[subject_labels == label])
-            sub_cope_data_FFA_mean = np.mean(sub_cope_data_FFA, axis=1)
-            cope_dict[roi]['{}_FFA_mean'.format(v)] = sub_cope_data_FFA_mean
+        for roi in rois:
+            roi_vertices = nib.freesurfer.read_label(roi_file.format(roi))
+            cope_data_roi = cope_data[:, roi_vertices]
+            if roi2label:
+                # get subgroup data
+                sub_cope_data_roi = np.atleast_2d(cope_data_roi[subject_labels == roi2label[roi]])
+                sub_cope_data_roi_mean = np.mean(sub_cope_data_roi, axis=1)
+                cope_dict[roi][v] = sub_cope_data_roi_mean
+            else:
+                cope_data_roi_mean = np.mean(cope_data_roi, axis=1)
+                cope_dict[roi][v] = cope_data_roi_mean
         print('Finished: {0}_{1}'.format(k, v))
 
     # output copes
+    cope_titles = interested_copes.values()
     for roi in cope_dict.keys():
-        with open(pjoin(n_clusters_dir, '{}_func_fingerprint.csv'.format(roi)), 'w+') as wf:
+        with open(pjoin(n_clusters_dir, '{}_func_fingerprint_test.csv'.format(roi)), 'w+') as wf:
             wf.write(','.join(cope_titles) + '\n')
             columns = []
             for title in cope_titles:
                 columns.append(cope_dict[roi][title])
             for row in zip(*columns):
-                row = [str(_) for _ in row]
+                row = map(str, row)
                 wf.write(','.join(row) + '\n')
--- FFA_action_pattern_analysis/4fingerprint_analysis.py
+++ FFA_action_pattern_analysis/4fingerprint_analysis.py
@@ -8,14 +8,13 @@ if __name__ == '__main__':
     # predefine some variates
     # -----------------------
     # predefine parameters
-    n_clusters = 3
-    subproject_name = '2mm_KM_init10_regress_right'
-    brain_structure = 'CIFTI_STRUCTURE_CORTEX_RIGHT'
-    roi2color = {'r1_FFA_m': 'b',
-                 'r2_FFA_p': 'r',
-                 'r2_FFA_a': 'g',
-                 'r3_FFA_p': 'y',
-                 'r3_FFA_a': 'k'}
+    show_errbar = False
+    rois = ['r1_FFA_m', 'r2_FFA_p', 'r2_FFA_a', 'r3_FFA_p', 'r3_FFA_a']
+    roi2color = {'r1_FFA_m': 'k', 'l1_FFA_m': 'k',
+                 'r2_FFA_p': 'r', 'l2_FFA_p': 'r',
+                 'r2_FFA_a': 'y', 'l2_FFA_a': 'y',
+                 'r3_FFA_p': 'b', 'l3_FFA_p': 'b',
+                 'r3_FFA_a': 'g', 'l3_FFA_a': 'g'}
     color2facecolor = {'b': 'blue',
                        'r': 'red',
                        'g': 'green',
@@ -23,30 +22,45 @@ if __name__ == '__main__':
                        'k': 'black'}
 
     # predefine paths
-    working_dir = '/nfs/s2/userhome/chenxiayu/workingdir'
-    project_dir = pjoin(working_dir, 'study/FFA_clustering')
-    subproject_dir = pjoin(project_dir, subproject_name)
-    n_clusters_dir = pjoin(subproject_dir, '{}clusters'.format(n_clusters))
+    project_dir = '/nfs/s2/userhome/chenxiayu/workingdir/study/FFA_clustering'
+    n_clusters_dir = pjoin(project_dir, '2mm_KM_init10_regress_right/3clusters')
     fingerprint_files = pjoin(n_clusters_dir, '{}_func_fingerprint.csv')
     # -----------------------
 
     fig, ax = plt.subplots()
     is_first_loop = True
-    for roi, color in roi2color.items():
+    for roi in rois:
         fingerprint_file = fingerprint_files.format(roi)
         reader = CsvReader(fingerprint_file)
         fingerprints = np.array(reader.rows[1:], dtype=np.float64)
 
         x = np.arange(fingerprints.shape[1])
         fingerprints_mean = np.mean(fingerprints, axis=0)
-        fingerprints_std = np.std(fingerprints, axis=0)
-        ax.plot(x, fingerprints_mean, '{}.-'.format(color), label=roi)
-        # ax.fill_between(x, fingerprints_mean - fingerprints_std, fingerprints_mean + fingerprints_std,
-        #                 alpha=0.5, facecolors=color2facecolor[color])
+        ax.plot(x, fingerprints_mean, '{}.-'.format(roi2color[roi]), label=roi)
+        if show_errbar:
+            fingerprints_std = np.std(fingerprints, axis=0)
+            ax.fill_between(x, fingerprints_mean - fingerprints_std, fingerprints_mean + fingerprints_std,
+                            alpha=0.5, facecolors=color2facecolor[roi2color[roi]])
         ax.legend()
 
         if is_first_loop:
             is_first_loop = False
-            plt.xticks(x, reader.rows[0])
+            xticklabels = np.array(reader.rows[0])
+            plt.xticks(x, xticklabels)
+            # plt.setp(ax.get_xticklabels(), rotation=45, ha='right', rotation_mode='anchor')
 
+    # fingerprint_file_1080 = '/nfs/t3/workingshop/chenxiayu/study/FFA_clustering/data/HCP_face-avg/label/' \
+    #                         'lFFA_2mm_func_fingerprint.csv'
+    # reader_1080 = CsvReader(fingerprint_file_1080)
+    # fingerprints_1080 = np.array(reader_1080.rows[1:], dtype=np.float64)
+    # x_1080 = np.arange(fingerprints_1080.shape[1])
+    # fingerprints_1080_mean = np.mean(fingerprints_1080, axis=0)
+    # ax.plot(x_1080, fingerprints_1080_mean, 'm*-', label='lFFA_2mm')
+    # if show_errbar:
+    #     fingerprints_1080_std = np.std(fingerprints_1080, axis=0)
+    #     ax.fill_between(x_1080, fingerprints_1080_mean - fingerprints_1080_std,
+    #                     fingerprints_1080_mean + fingerprints_1080_std,
+    #                     alpha=0.5, facecolors='fuchsia')
+
+    plt.tight_layout()
     plt.show()
