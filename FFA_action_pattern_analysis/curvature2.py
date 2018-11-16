0f4f8888df8d013c1271813c6147e686199901d9
sunshineDrizzle <954830460@qq.com>
2018-11-16 19:08:30 +0800

    Refine: neaten curvature2.py

--- FFA_action_pattern_analysis/curvature2.py
+++ FFA_action_pattern_analysis/curvature2.py
@@ -1,44 +1,56 @@
 if __name__ == '__main__':
+    import os
     import numpy as np
 
     from os.path import join as pjoin
     from commontool.io.io import CiftiReader, save2nifti
 
     # predefine some variates
-    n_clusters = 10
-    working_dir = '/nfs/s2/userhome/chenxiayu/workingdir'
-    project_dir = pjoin(working_dir, 'study/rFFA_clustering')
-    subproject_dir = pjoin(project_dir, '2mm_ward_regress')
-    n_clusters_dir = pjoin(subproject_dir, '{}clusters'.format(n_clusters))
+    # -----------------------
+    # predefine parameters
+    cluster_nums = range(1, 21)
+    hemi = 'rh'
 
-    # curv_reader = CiftiReader('/nfs/p1/public_dataset/datasets/hcp/DATA/HCP_S1200_GroupAvg_v1/'
-    #                           'HCP_S1200_GroupAvg_v1/S1200.All.curvature_MSMAll.32k_fs_LR.dscalar.nii')
-    curv_reader = CiftiReader(pjoin(project_dir, 'data/1080_curvature.dscalar.nii'))
-    curv_data = curv_reader.get_data('CIFTI_STRUCTURE_CORTEX_RIGHT', True)
+    # predefine paths
+    project_dir = '/nfs/s2/userhome/chenxiayu/workingdir/study/FFA_clustering'
+    cluster_num_dirs = pjoin(project_dir, '2mm_KM_zscore/{}clusters')
+    if hemi == 'lh':
+        brain_structure = 'CIFTI_STRUCTURE_CORTEX_LEFT'
+    elif hemi == 'rh':
+        brain_structure = 'CIFTI_STRUCTURE_CORTEX_RIGHT'
+    else:
+        raise RuntimeError("hemi must be one of ('lh', 'rh')!")
+    # -----------------------
+
+    # prepare data
+    curv_reader = CiftiReader(pjoin(project_dir, 'data/HCP_face-avg/S1200.1080.curvature_MSMAll.32k_fs_LR.dscalar.nii'))
+    curv_data = curv_reader.get_data(brain_structure, True)
     sulc_label = curv_data < 0
     curv_names = curv_reader.map_names()
 
-    curv_mean_maps = np.zeros((0, curv_data.shape[1]))
-    sulc_prob_maps = np.zeros((0, curv_data.shape[1]))
-    for label in range(1, n_clusters+1):
-        with open(pjoin(n_clusters_dir, 'subjects{}_id'.format(label))) as f:
-            subgroup_ids = f.read().splitlines()
-        subgroup_curv_names = [name + '_Curvature' for name in subgroup_ids]
+    # calculate subgroup curvature
+    for cluster_num in cluster_nums:
+        # get clustering labels of subjects
+        cluster_num_dir = cluster_num_dirs.format(cluster_num)
+        subject_labels_path = pjoin(cluster_num_dir, 'subject_labels')
+        with open(subject_labels_path) as rf:
+            subject_labels = np.array(rf.read().split(' '), dtype=np.uint16)
 
-        indices = []
-        for idx, name in enumerate(curv_names):
-            if name in subgroup_curv_names:
-                indices.append(idx)
+        curv_mean_maps = np.zeros((0, curv_data.shape[1]))
+        sulc_prob_maps = np.zeros((0, curv_data.shape[1]))
+        for label in sorted(set(subject_labels)):
+            subgroup_curv_data = curv_data[subject_labels == label]
+            subgroup_curv_data_mean = np.atleast_2d(np.mean(subgroup_curv_data, 0))
+            subgroup_sulc_prob = np.atleast_2d(np.mean(sulc_label[subject_labels == label], 0))
 
-        subgroup_curv_data = curv_data[indices, :]
-        subgroup_curv_data_mean = np.atleast_2d(np.mean(subgroup_curv_data, 0))
-        subgroup_sulc_prob = np.atleast_2d(np.mean(sulc_label[indices, :], 0))
+            curv_mean_maps = np.r_[curv_mean_maps, subgroup_curv_data_mean]
+            sulc_prob_maps = np.r_[sulc_prob_maps, subgroup_sulc_prob]
 
-        curv_mean_maps = np.r_[curv_mean_maps, subgroup_curv_data_mean]
-        sulc_prob_maps = np.r_[sulc_prob_maps, subgroup_sulc_prob]
+        # output
+        curv_dir = pjoin(cluster_num_dir, 'curvature')
+        if not os.path.exists(curv_dir):
+            os.makedirs(curv_dir)
+        save2nifti(pjoin(curv_dir, '{}_curv_mean_maps.nii.gz'.format(hemi)), curv_mean_maps)
+        # save2nifti(pjoin(curv_dir, '{}_sulc_prob_maps.nii.gz'.format(hemi)), sulc_prob_maps)
 
-        print(label, subgroup_curv_data.shape[0])
-
-    # output
-    save2nifti(pjoin(n_clusters_dir, 'curv_mean_maps_new.nii.gz'), curv_mean_maps)
-    # save2nifti(pjoin(n_clusters_dir, 'sulc_prob_maps.nii.gz'), sulc_prob_maps)
+        print('{}clusters: done'.format(cluster_num))
