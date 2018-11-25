b339dece0aca858fc426b562cdd1da71679e08de
sunshineDrizzle <954830460@qq.com>
2018-11-25 08:54:51 -0500

    EHN: refine codes and plot scalars on principal axis with errorbar

--- FFA_action_pattern_analysis/principal_axis3.py
+++ FFA_action_pattern_analysis/principal_axis3.py
@@ -1,66 +1,81 @@
 import numpy as np
-import nibabel as nib
 
 from os.path import join as pjoin
 from scipy import stats
+from commontool.io.io import CiftiReader
 
 
-def get_scalars_on_PA(src_file, PA, axes, color, label, zscore):
-    maps = nib.load(src_file).get_data()
+def PA_plot(src_file, brain_structures, PAs, subject_labels, axes, color, item, zscore):
 
-    x = np.arange(len(PA))
-    ys = maps[:, PA]
-    if zscore:
-        ys = stats.zscore(ys, 1)
-    for idx, y in enumerate(ys):
-        axes[idx].plot(x, y, color=color, label=label)
-        axes[idx].set_title(idx+1)
+    reader = CiftiReader(src_file)
+    for col in range(2):
+        PA = PAs[col]
+        x = np.arange(len(PA))
+        PA_maps = reader.get_data(brain_structures[col], True)[:, PA]
+        if zscore:
+            PA_maps = stats.zscore(PA_maps, 1)
+        for row, label in enumerate(sorted(set(subject_labels))):
+            subgroup_PA_maps = np.atleast_2d(PA_maps[subject_labels == label])
+            y = np.mean(subgroup_PA_maps, 0)
+            sem = stats.sem(subgroup_PA_maps, 0)
+            axes[row, col].errorbar(x, y, yerr=sem, color=color, label=item)
+            axes[row, col].set_title(label)
 
 
 if __name__ == '__main__':
+    import nibabel as nib
     import matplotlib.pyplot as plt
 
     cluster_num = 3
+    items = [
+        'activation',
+        # 'curvature',
+        'thickness',
+        # 'myelin'
+    ]
+
     project_dir = '/nfs/s2/userhome/chenxiayu/workingdir/study/FFA_clustering'
     cluster_num_dir = pjoin(project_dir, '2mm_KM_zscore/{}clusters'.format(cluster_num))
     lh_PA_file = pjoin(project_dir, 'data/HCP_face-avg/label/lFFA_2mm_PA.label')
     rh_PA_file = pjoin(project_dir, 'data/HCP_face-avg/label/rFFA_2mm_PA.label')
+    subject_labels_path = pjoin(cluster_num_dir, 'subject_labels')
 
     lh_PA = nib.freesurfer.read_label(lh_PA_file)[-1::-1]  # from posterior to anterior
     rh_PA = nib.freesurfer.read_label(rh_PA_file)[-1::-1]  # from posterior to anterior
+    with open(subject_labels_path) as rf:
+        subject_labels = np.array(rf.read().split(' '), dtype=np.uint16)
 
     fig, axes = plt.subplots(cluster_num, 2, sharex=True)
-    axes[0, 0].set_title('lh')
-    axes[0, 1].set_title('rh')
     axes[-1, 0].set_xlabel('from posterior to anterior of lFFA')
     axes[-1, 1].set_xlabel('from posterior to anterior of rFFA')
 
-    get_scalars_on_PA(pjoin(cluster_num_dir, 'activation/lh_zscore_mean_maps.nii.gz'),
-                      lh_PA, axes[:, 0], 'k', 'pattern', False)
-    get_scalars_on_PA(pjoin(cluster_num_dir, 'activation/rh_zscore_mean_maps.nii.gz'),
-                      rh_PA, axes[:, 1], 'k', 'pattern', False)
-
-    # get_scalars_on_PA(pjoin(cluster_num_dir, 'activation/lh_mean_maps.nii.gz'),
-    #                   lh_PA, axes[:, 0], 'y', 'activation', True)
-    # get_scalars_on_PA(pjoin(cluster_num_dir, 'activation/rh_mean_maps.nii.gz'),
-    #                   rh_PA, axes[:, 1], 'y', 'activation', True)
-    # get_scalars_on_PA(pjoin(cluster_num_dir, 'structure/lh_curv_mean_maps.nii.gz'),
-    #                   lh_PA, axes[:, 0], 'r', 'curvature', True)
-    # get_scalars_on_PA(pjoin(cluster_num_dir, 'structure/rh_curv_mean_maps.nii.gz'),
-    #                   rh_PA, axes[:, 1], 'r', 'curvature', True)
-    # get_scalars_on_PA(pjoin(cluster_num_dir, 'structure/lh_thickness_mean_maps.nii.gz'),
-    #                   lh_PA, axes[:, 0], 'g', 'thickness', True)
-    # get_scalars_on_PA(pjoin(cluster_num_dir, 'structure/rh_thickness_mean_maps.nii.gz'),
-    #                   rh_PA, axes[:, 1], 'g', 'thickness', True)
-    # get_scalars_on_PA(pjoin(cluster_num_dir, 'structure/lh_myelin_mean_maps.nii.gz'),
-    #                   lh_PA, axes[:, 0], 'b', 'myelin', True)
-    # get_scalars_on_PA(pjoin(cluster_num_dir, 'structure/rh_myelin_mean_maps.nii.gz'),
-    #                   rh_PA, axes[:, 1], 'b', 'myelin', True)
+    PAs = [lh_PA, rh_PA]
+    brain_structures = ['CIFTI_STRUCTURE_CORTEX_LEFT', 'CIFTI_STRUCTURE_CORTEX_RIGHT']
+    for item in items:
+        if item == 'pattern':
+            lh_src_file = pjoin(cluster_num_dir, 'activation/lh_zscore_mean_maps.nii.gz')
+            rh_src_file = pjoin(cluster_num_dir, 'activation/rh_zscore_mean_maps.nii.gz')
+            lmaps = nib.load(lh_src_file).get_data()
+            rmaps = nib.load(rh_src_file).get_data()
+            pass
+        elif item == 'activation':
+            src_file = pjoin(project_dir, 'data/HCP_face-avg/s2/S1200.1080.FACE-AVG_level2_zstat_hp200_s2_MSMAll.dscalar.nii')
+            PA_plot(src_file, brain_structures, PAs, subject_labels, axes, 'y', item, True)
+        elif item == 'curvature':
+            src_file = pjoin(project_dir, 'data/HCP_face-avg/S1200.1080.curvature_MSMAll.32k_fs_LR.dscalar.nii')
+            PA_plot(src_file, brain_structures, PAs, subject_labels, axes, 'r', item, True)
+        elif item == 'thickness':
+            src_file = pjoin(project_dir, 'data/HCP_face-avg/S1200_1080_thickness_MSMAll_32k_fs_LR.dscalar.nii')
+            PA_plot(src_file, brain_structures, PAs, subject_labels, axes, 'g', item, True)
+        elif item == 'myelin':
+            src_file = pjoin(project_dir, 'data/HCP_face-avg/S1200_1080_MyelinMap_BC_MSMAll_32k_fs_LR.dscalar.nii')
+            PA_plot(src_file, brain_structures, PAs, subject_labels, axes, 'b', item, True)
+        else:
+            raise RuntimeError("no such item: {}".format(item))
 
     for row in range(axes.shape[0]):
         for col in range(axes.shape[1]):
-            axes[row, col].set_title(row+1)
-            # axes[row, col].legend()
+            axes[row, col].legend()
 
     plt.tight_layout()
     plt.show()
