506cf56a25a0e6c5ab2e2b84f80944d232852cec
sunshineDrizzle <954830460@qq.com>
2019-01-09 09:05:35 -0500

    Refine: calc_mean_sem and plot_mean_sem

--- FFA_action_pattern_analysis/3activation.py
+++ FFA_action_pattern_analysis/3activation.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     # -----------------------
     # predefine parameters
     cluster_nums = [2]
-    hemi = 'lh'
+    hemi = 'rh'
     brain_structure = {
         'lh': 'CIFTI_STRUCTURE_CORTEX_LEFT',
         'rh': 'CIFTI_STRUCTURE_CORTEX_RIGHT'
@@ -46,6 +46,10 @@ if __name__ == '__main__':
         with open(group_labels_file) as rf:
             group_labels = np.array(rf.read().split(' '), dtype=np.uint16)
 
+        activation_dir = pjoin(cluster_num_dir, 'activation')
+        if not os.path.exists(activation_dir):
+            os.makedirs(activation_dir)
+
         stats_table_content = dict()
         for title in stats_table_titles:
             # initialize statistics table content
@@ -57,7 +61,7 @@ if __name__ == '__main__':
         pattern_mean_maps = np.zeros((0, maps.shape[1]))
         for label in sorted(set(group_labels)):
             # get subgroup data
-            subgroup_maps = maps[group_labels == label]
+            subgroup_maps = np.atleast_2d(maps[group_labels == label])
             subgroup_maps_mean = np.mean(subgroup_maps, 0)
             subgroup_maps_prob = np.mean(subgroup_maps > acti_thr, 0)
             subgroup_maps_numb = np.sum(subgroup_maps > acti_thr, 0)
@@ -83,6 +87,8 @@ if __name__ == '__main__':
             pattern_mean_map[:, FFA_vertices] = subgroup_FFA_patterns_mean
             pattern_mean_maps = np.r_[pattern_mean_maps, pattern_mean_map]
 
+            save2nifti(pjoin(activation_dir, '{}{}_maps.nii.gz'.format(hemi[0], label)), subgroup_maps)
+
         max_num_map = np.argmax(num_maps, 0) + 1
         max_prob_map = np.argmax(prob_maps, 0) + 1
         top_prob_ROIs = (prob_maps > prob_thr).astype(np.int8)
@@ -95,9 +101,6 @@ if __name__ == '__main__':
                 top_acti_ROIs[row, col] = 1
 
         # output maps
-        activation_dir = pjoin(cluster_num_dir, 'activation')
-        if not os.path.exists(activation_dir):
-            os.makedirs(activation_dir)
         header = nib.Nifti2Header()
         header['descrip'] = 'FreeROI label'
         # save2nifti(pjoin(activation_dir, '{}_mean_maps.nii.gz'.format(hemi)), mean_maps)
@@ -105,8 +108,8 @@ if __name__ == '__main__':
         # save2nifti(pjoin(activation_dir, 'max_num_map_z{}.nii.gz'.format(acti_thr)), max_num_map)
         # save2nifti(pjoin(activation_dir, 'max_prob_map_z{}.nii.gz'.format(acti_thr)), max_prob_map)
         # save2nifti(pjoin(activation_dir, 'top_prob_ROIs_z{}_p{}.nii.gz'.format(acti_thr, prob_thr)), top_prob_ROIs)
-        save2nifti(pjoin(activation_dir, '{}_top_acti_ROIs_percent{}.nii.gz'.format(hemi, int(top_acti_percent*100))),
-                   top_acti_ROIs, header=header)
+        # save2nifti(pjoin(activation_dir, '{}_top_acti_ROIs_percent{}.nii.gz'.format(hemi, int(top_acti_percent*100))),
+        #            top_acti_ROIs, header=header)
         # save2nifti(pjoin(activation_dir, '{}_pattern_mean_maps.nii.gz'.format(hemi)), pattern_mean_maps)
 
         # output statistics
--- FFA_action_pattern_analysis/5tseries_connect_analysis.py
+++ FFA_action_pattern_analysis/5tseries_connect_analysis.py
@@ -23,101 +23,71 @@ def calc_mean_map(connect_files, items):
         save2nifti(pjoin(mean_dir, out_name), mean)
 
 
-def calc_mean_sem(connect_files, items, roi_map_file, labels_l=None, labels_r=None):
+def calc_mean_sem(maps, output_file, mask=None, label_names=None):
     """
     calculate mean and sem across subjects
-
-    :param connect_files:
-    :param items:
-    :param roi_map_file:
-    :param labels_l:
-    :param labels_r:
-    :return:
     """
-    connect_dir = os.path.dirname(connect_files)
-    mean_sem_dir = pjoin(connect_dir, 'mean_sem')
-    if not os.path.exists(mean_sem_dir):
-        os.makedirs(mean_sem_dir)
+    vtx_num = maps.shape[1]
+    if mask is None:
+        label_ids = list(map(str, range(vtx_num)))
+        trg_vertices_list = [[vtx] for vtx in range(vtx_num)]
+    else:
+        label_ids = [int(label_id) for label_id in np.unique(mask) if label_id != 0]
+        trg_vertices_list = [np.where(mask == i)[0] for i in label_ids]
+        label_ids = list(map(str, label_ids))
+    if label_names is None:
+        label_names = label_ids.copy()
+    else:
+        assert len(label_ids) == len(label_names)
+    label_ids.insert(0, 'label_id')
+    label_names.insert(0, 'label_name')
 
-    dict_lr = dict()
-    if roi_map_file is not None:
-        reader = CiftiReader(roi_map_file)
-        roi_map_l = reader.get_data('CIFTI_STRUCTURE_CORTEX_LEFT', True)
-        roi_map_r = reader.get_data('CIFTI_STRUCTURE_CORTEX_RIGHT', True)
+    means = ['mean']
+    sems = ['sem']
+    for trg_vertices in trg_vertices_list:
+        trg_data = np.mean(maps[:, trg_vertices], 1)
+        means.append(str(np.mean(trg_data)))
+        sems.append(str(sem(trg_data)))
 
-        if labels_l is None:
-            labels_l = [int(label) for label in np.unique(roi_map_l) if label != 0]
-        else:
-            labels_l = [int(label) for label in labels_l if label != 0]
-        if labels_r is None:
-            labels_r = [int(label) for label in np.unique(roi_map_r) if label != 0]
-        else:
-            labels_r = [int(label) for label in labels_r if label != 0]
-
-        dict_lr['trg_vertices_l'] = [np.where(roi_map_l == i)[1] for i in labels_l]
-        dict_lr['trg_vertices_r'] = [np.where(roi_map_r == i)[1] for i in labels_r]
-        dict_lr['label_name_l'] = [reader.label_tables()[0][i].label for i in labels_l]
-        dict_lr['label_name_r'] = [reader.label_tables()[0][i].label for i in labels_r]
-        dict_lr['label_l'] = [str(i) for i in labels_l]
-        dict_lr['label_r'] = [str(i) for i in labels_r]
-
-    for item in items:
-        connect_file = connect_files.format(item=item)
-        data = np.atleast_2d(nib.load(connect_file).get_data())
-        if dict_lr:
-            trg_vertices_list = dict_lr['trg_vertices_' + item[-2]]
-            label_names = dict_lr['label_name_' + item[-2]].copy()
-            labels = dict_lr['label_' + item[-2]].copy()
-        else:
-            trg_vertices_list = [[vtx] for vtx in range(data.shape[1])]
-            label_names = list(range(data.shape[1]))
-            labels = list(range(data.shape[1]))
-        means = ['mean']
-        sems = ['sem']
-        for trg_vertices in trg_vertices_list:
-            trg_data = np.mean(data[:, trg_vertices], 1)
-            means.append(str(np.mean(trg_data)))
-            sems.append(str(sem(trg_data)))
-        label_names.insert(0, 'label_name')
-        labels.insert(0, 'label')
-
-        line1 = ','.join(label_names)
-        line2 = ','.join(means)
-        line3 = ','.join(sems)
-        line4 = ','.join(labels)
-        lines = '\n'.join([line1, line2, line3, line4])
-        open(pjoin(mean_sem_dir, item), 'w+').writelines(lines)
+    line1 = ','.join(label_ids)
+    line2 = ','.join(label_names)
+    line3 = ','.join(means)
+    line4 = ','.join(sems)
+    lines = '\n'.join([line1, line2, line3, line4])
+    open(output_file, 'w+').writelines(lines)
 
 
-def plot_mean_sem(mean_sem_files, items, labels=None):
+def plot_mean_sem(mean_sem_files, items=None, label_ids=None, xlabel='', ylabel=''):
 
     fig, ax = plt.subplots()
     x = None
     width = None
     xticklabels = None
     rects_list = []
-    item_num = len(items)
-    for idx, item in enumerate(items):
-        mean_sem_file = mean_sem_files.format(item)
+    item_num = len(mean_sem_files)
+    for idx, mean_sem_file in enumerate(mean_sem_files):
         mean_sem_dict = CsvReader(mean_sem_file).to_dict(1)
-        if labels is None:
-            labels = mean_sem_dict['label']
+        if label_ids is None:
+            label_ids = mean_sem_dict['label_id']
         else:
-            labels = [str(i) for i in labels]
+            label_ids = [str(i) for i in label_ids]
         if x is None:
-            xticklabels = [mean_sem_dict['label_name'][mean_sem_dict['label'].index(i)] for i in labels]
-            x = np.arange(len(labels))
+            xticklabels = [mean_sem_dict['label_name'][mean_sem_dict['label_id'].index(i)] for i in label_ids]
+            x = np.arange(len(label_ids))
             width = auto_bar_width(x, item_num)
-        y = [float(mean_sem_dict['mean'][mean_sem_dict['label'].index(i)]) for i in labels]
-        sems = [float(mean_sem_dict['sem'][mean_sem_dict['label'].index(i)]) for i in labels]
+        y = [float(mean_sem_dict['mean'][mean_sem_dict['label_id'].index(i)]) for i in label_ids]
+        sems = [float(mean_sem_dict['sem'][mean_sem_dict['label_id'].index(i)]) for i in label_ids]
         rects = ax.bar(x+width*idx, y, width, color='k', alpha=1./((idx+1)/2+0.5), yerr=sems)
         rects_list.append(rects)
-    ax.legend(rects_list, items)
+    if items is not None:
+        assert item_num == len(items)
+        ax.legend(rects_list, items)
     ax.set_xticks(x+width/2.0*(item_num-1))
     ax.set_xticklabels(xticklabels)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    ax.set_ylabel('pearson r')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
     plt.setp(ax.get_xticklabels(), rotation=-90, ha='left', rotation_mode='anchor')
 
     plt.tight_layout()
@@ -238,8 +208,13 @@ def plot_compare(compare_file, labels=None, p_thr=None):
 
 if __name__ == '__main__':
 
+    brain_structure = {
+        'l': 'CIFTI_STRUCTURE_CORTEX_LEFT',
+        'r': 'CIFTI_STRUCTURE_CORTEX_RIGHT'
+    }
+
     project_dir = '/nfs/s2/userhome/chenxiayu/workingdir/study/FFA_clustering'
-    connect_dir = pjoin(project_dir, 's2_25_zscore/HAC_ward_euclidean/2clusters/rfMRI_connectivity/glasser_mmp')
+    connect_dir = pjoin(project_dir, 's2_25_zscore/HAC_ward_euclidean/2clusters/rfMRI_connectivity/glasser_mmp_bak')
     mask_roi_file_l = pjoin(project_dir, 'data/HCP_1080/face-avg_s2/label/mmp_prob2.3_25_intersect_l.nii.gz')
     mask_roi_file_r = pjoin(project_dir, 'data/HCP_1080/face-avg_s2/label/mmp_prob2.3_25_intersect_r.nii.gz')
     mask_rois_l = nib.load(mask_roi_file_l).get_data()
@@ -258,15 +233,26 @@ if __name__ == '__main__':
     #          'r1_FFA2_connect_l1', 'r1_FFA2_connect_r1']
     # calc_mean_map(connect_files, items)
 
-    # items = ['r1_FFA1_connect_l1', 'r1_FFA1_connect_l2', 'r1_FFA1_connect_r1', 'r1_FFA1_connect_r2',
-    #          'r1_FFA2_connect_l1', 'r1_FFA2_connect_l2', 'r1_FFA2_connect_r1', 'r1_FFA2_connect_r2',
-    #          'r2_FFA1_connect_l2', 'r2_FFA1_connect_l1', 'r2_FFA1_connect_r2', 'r2_FFA1_connect_r1',
-    #          'l1_FFA1_connect_l1', 'l1_FFA1_connect_l2', 'l1_FFA1_connect_r1', 'l1_FFA1_connect_r2',
-    #          'l1_FFA2_connect_l1', 'l1_FFA2_connect_l2', 'l1_FFA2_connect_r1', 'l1_FFA2_connect_r2',
-    #          'l2_FFA1_connect_l2', 'l2_FFA1_connect_l1', 'l2_FFA1_connect_r2', 'l2_FFA1_connect_r1']
-    # calc_mean_sem(pjoin(connect_dir, '{item}.nii.gz'), items,
-    #               '/nfs/p1/atlases/multimodal_glasser/surface/MMP_mpmLR32k.dlabel.nii')
+    # --------------------------------------calc_mean_sem start---------------------------------------------
+    items = ['r1_FFA1_connect_l1', 'r1_FFA1_connect_l2', 'r1_FFA1_connect_r1', 'r1_FFA1_connect_r2',
+             'r1_FFA2_connect_l1', 'r1_FFA2_connect_l2', 'r1_FFA2_connect_r1', 'r1_FFA2_connect_r2',
+             'r2_FFA1_connect_l2', 'r2_FFA1_connect_l1', 'r2_FFA1_connect_r2', 'r2_FFA1_connect_r1',
+             'l1_FFA1_connect_l1', 'l1_FFA1_connect_l2', 'l1_FFA1_connect_r1', 'l1_FFA1_connect_r2',
+             'l1_FFA2_connect_l1', 'l1_FFA2_connect_l2', 'l1_FFA2_connect_r1', 'l1_FFA2_connect_r2',
+             'l2_FFA1_connect_l2', 'l2_FFA1_connect_l1', 'l2_FFA1_connect_r2', 'l2_FFA1_connect_r1']
+    mean_sem_dir = pjoin(connect_dir, 'mean_sem_new')
+    if not os.path.exists(mean_sem_dir):
+        os.makedirs(mean_sem_dir)
+    reader = CiftiReader('/nfs/p1/atlases/multimodal_glasser/surface/MMP_mpmLR32k.dlabel.nii')
+    for item in items:
+        maps = np.atleast_2d(nib.load(pjoin(connect_dir, '{}.nii.gz'.format(item))).get_data())
+        mask = reader.get_data(brain_structure[item[-2]], True).ravel()
+        label_ids = [int(label_id) for label_id in np.unique(mask) if label_id != 0]
+        label_names = [reader.label_tables()[0][i].label for i in label_ids]
+        calc_mean_sem(maps, pjoin(mean_sem_dir, item), mask, label_names)
+    # --------------------------------------calc_mean_sem end---------------------------------------------
 
+    # --------------------------------------plot_mean_sem start---------------------------------------------
     # intersubgroup_items_list = [
     #     ['l1_FFA1_connect_l1', 'l1_FFA1_connect_l2'],
     #     ['l1_FFA1_connect_r1', 'l1_FFA1_connect_r2'],
@@ -281,18 +267,21 @@ if __name__ == '__main__':
     #     ['r2_FFA1_connect_l2', 'r2_FFA1_connect_l1'],
     #     ['r2_FFA1_connect_r2', 'r2_FFA1_connect_r1']
     # ]
-    # interroi_items_list = [
-    #     ['l1_FFA1_connect_l1', 'l2_FFA1_connect_l1', 'l1_FFA2_connect_l1'],
-    #     ['l1_FFA1_connect_r1', 'l2_FFA1_connect_r1', 'l1_FFA2_connect_r1'],
-    #     ['r1_FFA1_connect_l1', 'r2_FFA1_connect_l1', 'r1_FFA2_connect_l1'],
-    #     ['r1_FFA1_connect_r1', 'r2_FFA1_connect_r1', 'r1_FFA2_connect_r1'],
-    #     ['l1_FFA1_connect_l2', 'l2_FFA1_connect_l2', 'l1_FFA2_connect_l2'],
-    #     ['l1_FFA1_connect_r2', 'l2_FFA1_connect_r2', 'l1_FFA2_connect_r2'],
-    #     ['r1_FFA1_connect_l2', 'r2_FFA1_connect_l2', 'r1_FFA2_connect_l2'],
-    #     ['r1_FFA1_connect_r2', 'r2_FFA1_connect_r2', 'r1_FFA2_connect_r2']
-    # ]
-    # for items in interroi_items_list:
-    #     plot_mean_sem(pjoin(connect_dir, 'mean_sem/{}'), items, labels_dict[items[0][-2]])
+    interroi_items_list = [
+        ['l1_FFA1_connect_l1', 'l2_FFA1_connect_l1', 'l1_FFA2_connect_l1'],
+        ['l1_FFA1_connect_r1', 'l2_FFA1_connect_r1', 'l1_FFA2_connect_r1'],
+        ['r1_FFA1_connect_l1', 'r2_FFA1_connect_l1', 'r1_FFA2_connect_l1'],
+        ['r1_FFA1_connect_r1', 'r2_FFA1_connect_r1', 'r1_FFA2_connect_r1'],
+        ['l1_FFA1_connect_l2', 'l2_FFA1_connect_l2', 'l1_FFA2_connect_l2'],
+        ['l1_FFA1_connect_r2', 'l2_FFA1_connect_r2', 'l1_FFA2_connect_r2'],
+        ['r1_FFA1_connect_l2', 'r2_FFA1_connect_l2', 'r1_FFA2_connect_l2'],
+        ['r1_FFA1_connect_r2', 'r2_FFA1_connect_r2', 'r1_FFA2_connect_r2']
+    ]
+    mean_sem_dir = pjoin(connect_dir, 'mean_sem_new')
+    for items in interroi_items_list:
+        mean_sem_files = [pjoin(mean_sem_dir, item) for item in items]
+        plot_mean_sem(mean_sem_files, items, labels_dict[items[0][-2]], ylabel='pearson r')
+    # --------------------------------------plot_mean_sem end---------------------------------------------
 
     # intersubgroup_item_pairs = [
     #     ['l1_FFA1_connect_l1', 'l1_FFA1_connect_l2'],
@@ -308,26 +297,50 @@ if __name__ == '__main__':
     #     ['r2_FFA1_connect_l2', 'r2_FFA1_connect_l1'],
     #     ['r2_FFA1_connect_r2', 'r2_FFA1_connect_r1']
     # ]
-    interroi_item_pairs = [
-        ['l2_FFA1_connect_l1', 'l1_FFA1_connect_l1'],
-        ['l2_FFA1_connect_l1', 'l1_FFA2_connect_l1'],
-        ['l2_FFA1_connect_r1', 'l1_FFA1_connect_r1'],
-        ['l2_FFA1_connect_r1', 'l1_FFA2_connect_r1'],
-        ['r2_FFA1_connect_l1', 'r1_FFA1_connect_l1'],
-        ['r2_FFA1_connect_l1', 'r1_FFA2_connect_l1'],
-        ['r2_FFA1_connect_r1', 'r1_FFA1_connect_r1'],
-        ['r2_FFA1_connect_r1', 'r1_FFA2_connect_r1'],
-        ['l2_FFA1_connect_l2', 'l1_FFA1_connect_l2'],
-        ['l2_FFA1_connect_l2', 'l1_FFA2_connect_l2'],
-        ['l2_FFA1_connect_r2', 'l1_FFA1_connect_r2'],
-        ['l2_FFA1_connect_r2', 'l1_FFA2_connect_r2'],
-        ['r2_FFA1_connect_l2', 'r1_FFA1_connect_l2'],
-        ['r2_FFA1_connect_l2', 'r1_FFA2_connect_l2'],
-        ['r2_FFA1_connect_r2', 'r1_FFA1_connect_r2'],
-        ['r2_FFA1_connect_r2', 'r1_FFA2_connect_r2']
-    ]
+    # interroi_item_pairs = [
+    #     ['l2_FFA1_connect_l1', 'l1_FFA1_connect_l1'],
+    #     ['l2_FFA1_connect_l1', 'l1_FFA2_connect_l1'],
+    #     ['l2_FFA1_connect_r1', 'l1_FFA1_connect_r1'],
+    #     ['l2_FFA1_connect_r1', 'l1_FFA2_connect_r1'],
+    #     ['r2_FFA1_connect_l1', 'r1_FFA1_connect_l1'],
+    #     ['r2_FFA1_connect_l1', 'r1_FFA2_connect_l1'],
+    #     ['r2_FFA1_connect_r1', 'r1_FFA1_connect_r1'],
+    #     ['r2_FFA1_connect_r1', 'r1_FFA2_connect_r1'],
+    #     ['l2_FFA1_connect_l2', 'l1_FFA1_connect_l2'],
+    #     ['l2_FFA1_connect_l2', 'l1_FFA2_connect_l2'],
+    #     ['l2_FFA1_connect_r2', 'l1_FFA1_connect_r2'],
+    #     ['l2_FFA1_connect_r2', 'l1_FFA2_connect_r2'],
+    #     ['r2_FFA1_connect_l2', 'r1_FFA1_connect_l2'],
+    #     ['r2_FFA1_connect_l2', 'r1_FFA2_connect_l2'],
+    #     ['r2_FFA1_connect_r2', 'r1_FFA1_connect_r2'],
+    #     ['r2_FFA1_connect_r2', 'r1_FFA2_connect_r2']
+    # ]
     # compare(pjoin(connect_dir, '{item}.nii.gz'), interroi_item_pairs,
     #         '/nfs/p1/atlases/multimodal_glasser/surface/MMP_mpmLR32k.dlabel.nii')
 
-    for item0, item1 in interroi_item_pairs:
-        plot_compare(pjoin(connect_dir, 'compare/{}_vs_{}'.format(item0, item1)), labels_dict[item0[-2]], 0.05)
+    # for item0, item1 in interroi_item_pairs:
+    #     plot_compare(pjoin(connect_dir, 'compare/{}_vs_{}'.format(item0, item1)), labels_dict[item0[-2]], 0.05)
+
+    # items = ['maps_l1', 'maps_l2', 'maps_r1', 'maps_r2']
+    # calc_mean_sem(pjoin(project_dir, 's2_25_zscore/HAC_ward_euclidean/2clusters/activation/{item}.nii.gz'), items,
+    #               '/nfs/p1/atlases/multimodal_glasser/surface/MMP_mpmLR32k.dlabel.nii')
+    #
+    # items_list = [
+    #     ['maps_l1', 'maps_l2'],
+    #     ['maps_r1', 'maps_r2']
+    # ]
+    # for items in items_list:
+    #     plot_mean_sem(pjoin(project_dir, 's2_25_zscore/HAC_ward_euclidean/2clusters/activation/mean_sem/{}'),
+    #                   items)
+    #
+    # acti_items = [
+    #     ['maps_l1', 'maps_l2'],
+    #     ['maps_r1', 'maps_r2']
+    # ]
+    # compare(pjoin(project_dir, 's2_25_zscore/HAC_ward_euclidean/2clusters/activation/{item}.nii.gz'),
+    #         acti_items,
+    #         '/nfs/p1/atlases/multimodal_glasser/surface/MMP_mpmLR32k.dlabel.nii', p_thr=0.05)
+
+    # for item0, item1 in acti_items:
+    #     plot_compare(pjoin(project_dir, 's2_25_zscore/HAC_ward_euclidean/2clusters/activation/compare/{}_vs_{}'.format(item0, item1)),
+    #                  p_thr=0.05)
