4bed709ed87811e955ff5c0dd619d995762a2480
sunshineDrizzle <954830460@qq.com>
2019-01-22 09:43:57 -0500

    tseries_connect

---
+++ FFA_action_pattern_analysis/5tseries_connect.py
@@ -0,0 +1,34 @@
+
+
+if __name__ == '__main__':
+    import numpy as np
+
+    from os.path import join as pjoin
+    from scipy import stats
+
+    project_dir = '/nfs/s2/userhome/chenxiayu/workingdir/study/FFA_clustering'
+    connect_dir = pjoin(project_dir, 's2_25_zscore/HAC_ward_euclidean/2clusters/rfMRI_connectivity/PAM_z165_p025_ROI')
+    subject_ids_file = pjoin(connect_dir, 'subject_id1_4run_1200')
+    output_file = pjoin(connect_dir, 'subgroup1_connectivity.npy')
+    tseries_LR_file = pjoin(connect_dir, '{subject}/rfMRI_REST1_LR.npy')
+    tseries_RL_file = pjoin(connect_dir, '{subject}/rfMRI_REST1_RL.npy')
+
+    subject_ids = np.array(open(subject_ids_file).read().splitlines())
+
+    subgroup_connectivity = []
+    for subject in subject_ids:
+        tseries_LR = np.load(tseries_LR_file.format(subject=subject))
+        tseries_RL = np.load(tseries_RL_file.format(subject=subject))
+        tseries_LR = stats.zscore(tseries_LR, 1)
+        tseries_RL = stats.zscore(tseries_RL, 1)
+        tseries = np.c_[tseries_LR, tseries_RL]
+        site_num = tseries.shape[0]
+        subject_connectivity = np.zeros((site_num, site_num))
+        for i in range(site_num):
+            for j in range(i, site_num):
+                r = stats.pearsonr(tseries[i], tseries[j])[0]
+                subject_connectivity[i, j] = r
+                subject_connectivity[j, i] = r
+        subgroup_connectivity.append(subject_connectivity)
+        print(subject)
+    np.save(output_file, np.array(subgroup_connectivity))
